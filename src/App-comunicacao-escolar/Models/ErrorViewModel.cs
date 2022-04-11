62bcc0748e71b35b3b4ac8d044863fc2bd759dce
Sergio Menezes <74699119+sergiomenezesfilho@users.noreply.github.com>
2022-04-11 05:22:14 -0300

    Delete src/App-comunicacao-escolar directory

--- src/App-comunicacao-escolar/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace App_comunicacao_escolar.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
